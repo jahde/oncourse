d563f6f29eabef08d6bef20b56d9ee57689df1e0
Jahde <jahde.hiywan@gmail.com>
2014-11-09 22:49:22 -0500

    added unit tests

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ActiveRecord::Base
+
+  validates :title, :presence => true
+
 end
+
+# rails g scaffold courses
+# title:string description:text price:decimal seats:integer
+# starts_at:datetime ends_at:datetime
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "must not save if the title is missing" do
+
+    course = Course.new
+    course.save
+    assert course.errors[:title].include? "can't be blank"
+
+
+  end
+
 end
+
